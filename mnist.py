1f0da13373aa74e9d36b41aee95c8b601d6bba53
Rahul Yadav <61009919+Rahuly-adav@users.noreply.github.com>
2020-05-26 13:03:25 +0530

    Create mnist.py

---
+++ mnist.py
@@ -0,0 +1,33 @@
+# Baseline MLP for MNIST dataset
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+units=3
+# define baseline model
+model = Sequential()
+model.add(Dense(units, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=200, verbose=0)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+with open ("file1.txt",'w') as f1:
+	f1.write(str(int(scores[1]*100)))
+
+
+
